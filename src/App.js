a87caf3238475f03d27a5c3a7525cd3a8ade0048
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-11-05 15:30:45 -0500

    start of individual work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Quote from "./components/quotes";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-            <Quote />
-        </div>
-    );
+class App extends Component {
+    state = {
+        category: "dev"
+    };
+
+    changeCategory = () => {
+        this.setState({
+            category: "sport"
+        });
+    };
+
+    // Move Change Category into it's own component and make it a drop down menu (<select> element )
+
+    render() {
+        const { category } = this.state;
+        return (
+            <div className="App">
+                <Quote category={category} />
+                <button onClick={() => this.changeCategory()}>
+                    Change Category
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -10,18 +10,40 @@ class Quote extends Component {
         this.getQuote();
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.category !== this.props.category) {
+            this.getQuote();
+        }
+    }
+
     getQuote = async () => {
-        const data = await loadData("https://api.chucknorris.io/jokes/random?category=dev");
+        const { category } = this.props
+        const data = await loadData(
+            `https://api.chucknorris.io/jokes/random?category=${category}`
+        );
         const quote = data.value;
 
         this.setState({
             quote
-        })
+        });
     };
 
+    handleClick = () => {
+        this.getQuote();
+    }
+    
     render() {
         const { quote } = this.state;
-        return <p>{quote}</p>;
+        const { category } = this.props;
+
+        return (
+            <>
+                <p>{quote}</p>
+                <button onClick={() => this.handleClick()}>
+                    Get Another Quote from the {category} Category!
+                </button>
+            </>
+        );
     }
 }
 
